82089824ccddcc1060070fd6c65117843eb8d0ad
Frankie Truong <frankie.truong@leonardo.com>
2017-10-20 10:42:37 -0400

    App.java edited online with Bitbucket

--- src/main/java/com/mycompany/testproject/App.java
+++ src/main/java/com/mycompany/testproject/App.java
@@ -1,15 +1,1 @@
-package com.mycompany.testproject;
 
-test5
-/**
- * Hello world!!!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-
-}

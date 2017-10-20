a49f5c1ba107c09286b80df063450aab18a98060
Frankie Truong <frankie.truong@leonardo.com>
2017-10-20 10:44:19 -0400

    RTR-3779 Test

--- artifacts.sh
+++ artifacts.sh
@@ -1,17 +1,2 @@
 # Artifactory location
-test
-server=http://artifactory.visualfrenzy.com/artifactory
-repo=libs-snapshot-local
-
-# Maven artifact location
-name=testProject
-artifact=com/mycompany/$name
-path=$server/$repo/$artifact
-version=`curl -s $path/maven-metadata.xml | grep latest | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/"`
-build=`curl -s $path/$version/maven-metadata.xml | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
-jar=$name-$build.jar
-url=$path/$version/$jar
-
-# Download qwei35n342344k
-echo $url
-wget -q -N $url
+YO DAWG 
